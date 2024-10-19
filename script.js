76af59b1604b1eaec9e170c5703482b45a05131a
_vision <shefroy.rowe@gmail.com>
2024-10-19 12:13:10 -0400

    impliment initial stages of add book to library function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const container = document.querySelector('main');
+
+const myLibrary = []; //store books
+
 //book constructor
 function Book(title, author, pages, read) {
     this.title = title;
@@ -5,6 +9,34 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     this.bookInfo = () => {
-            return `"${this.title}, ${this.author}, ${this.pages}, ${this.read}"`;
-        }
+        return `"${this.title}, ${this.author}, ${this.pages}, ${this.read}"`;
+    }
+}
+
+function addBookToLibrary() {
+    //create new book
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);//push book to library
+
+    //create html book elements
+    const card = document.createElement('div');
+    const cardTitle = document.createElement('h1');
+    const cardAuthor = document.createElement('h3');
+    const cardpages = document.createElement('span');
+    const cardRead = document.createElement('span');
+
+    //asign values to and append elements
+    cardTitle.textContent = book.title;
+    cardAuthor.textContent = book.author;
+    cardpages.textContent = book.pages;
+    cardRead.textContent = book.read;
+
+    //append elements to card
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardpages);
+    card.appendChild(cardRead);
+
+    //append elements to dom
+    container.innerHTML += card;
 }
